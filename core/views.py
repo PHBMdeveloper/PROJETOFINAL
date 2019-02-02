92bea1484366c9fbbf418e914fad16a743df41f8
PHDeveloper <phbm@hotmail.com>
2019-02-02 16:32:17 -0200

    corrigindo problema

--- core/views.py
+++ core/views.py
@@ -7,17 +7,7 @@ from .models import (
     MovMensalista
 )
 
-<<<<<<< HEAD
 from .form import PessoaForm, VeiculoForm
-=======
-from .form import (
-    PessoaForm,
-    VeiculoForm,
-    MovRotativoForm,
-    MensalistaForm,
-    MovmensalistaForm
-)
->>>>>>> HOMEOFFICE
 
 
 def home(request):
@@ -33,7 +23,6 @@ def lista_pessoas(request):
 
 
 def pessoa_novo(request):
-<<<<<<< HEAD
         form = PessoaForm(request.POST or None)
         if form.is_valid():
                 form.save()
@@ -51,76 +40,6 @@ def veiculo_novo(request):
         if form.is_valid():
                 form.save()
         return redirect('core_lista_veiculo')
-=======
-    form = PessoaForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    return redirect('core_lista_pessoa')
-
-
-def pessoa_update(request, id):
-    data = {}
-    pessoa = Pessoa.objects.get(id=id)
-    form = PessoaForm(request.POST or None, instance=pessoa)
-    data['pessoa'] = pessoa
-    data['form'] = form
-
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('core_lista_pessoa')
-    else:
-        return render(request, 'core/update_pessoa.html', data)
-
-
-def pessoa_delete(request, id):
-    pessoa = Pessoa.objects.get(id=id)
-    if request.method == 'POST':
-        pessoa.delete()
-        return redirect('core_lista_pessoa')
-    else:
-        return render(request, 'core/delete_confirm.html', {'obj': pessoa})
-
-
-def lista_veiculos(request):
-    form = VeiculoForm()
-    veiculos = Veiculo.objects.all
-    data = {'veiculos': veiculos, 'form': form}
-    return render(request, 'core/lista_veiculos.html', data)
-
-
-def veiculo_novo(request):
-    form = VeiculoForm(request.POST or None)
-    if form.is_valid():
-            form.save()
-    return redirect('core_lista_veiculo')
-
-
-def veiculo_update(request, id):
-    data = {}
-    veiculo = Veiculo.objects.get(id=id)
-    form = VeiculoForm(request.POST or None, instance=veiculo)
-    data['veiculo'] = veiculo
-    data['form'] = form
-
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('core_lista_veiculo')
-    else:
-        return render(request, 'core/update_veiculo.html', data)
-
-
-def veiculo_delete(request, id):
-    veiculo = Veiculo.objects.get(id=id)
-    if request.method == 'POST':
-        veiculo.delete()
-        return redirect('core_lista_veiculo')
-    else:
-        return render(
-            request, 'core/delete_confirm.html', {'obj': veiculo}
-        )
->>>>>>> HOMEOFFICE
 
 
 def lista_movrotativos(request):
