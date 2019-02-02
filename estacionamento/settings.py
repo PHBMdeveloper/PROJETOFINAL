08dacd7c25a1ee5bd19fab103fb47f9f967ef3d8
PHDeveloper <phbm@hotmail.com>
2019-02-02 16:20:38 -0200

    corrigindo problema DisallowedHost

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['estacionamento748.herokuapp.com/']
+ALLOWED_HOSTS = ['estacionamento748.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
