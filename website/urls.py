19d86625d833b70e5a7060505338a71223c393fe
PHDeveloper <phbm@hotmail.com>
2019-02-08 18:00:09 -0200

    Tarefa 1

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include, re_path
-from .views import home, contato
+from .views import home, contato, servicos, sobre, planos
 
 
 urlpatterns = [
     path('', home, name='website_home'),
     path('contato', contato, name='website_contato'),
+    path('servicos', servicos, name='website_servicos'),
+    path('sobre', sobre, name='website_sobre'),
+    path('planos', planos, name='website_planos'),
 ]
